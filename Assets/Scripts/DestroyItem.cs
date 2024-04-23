496b2629e801d4fb6809bb39375842f67cb52c81
bollemedost <iluck23@student.aau.dk>
2024-04-23 11:21:11 +0200

    Items To Find List

--- Assets/Scripts/DestroyItem.cs
+++ Assets/Scripts/DestroyItem.cs
@@ -6,6 +6,12 @@ public class DestroyItem : MonoBehaviour
 {
     private void OnMouseDown()
     {
+        // Destroy all child GameObjects
+        foreach (Transform child in transform)
+        {
+            Destroy(child.gameObject);
+        }
+
         // Destroy the GameObject when it's clicked
         Destroy(gameObject);
     }
