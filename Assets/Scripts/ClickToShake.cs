ed889c65f59047555e1a0d8abea660bf48bc996f
bollemedost <iluck23@student.aau.dk>
2024-04-23 14:35:17 +0200

    Don't Touch Items Shake

---
+++ Assets/Scripts/ClickToShake.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickToShake : MonoBehaviour
+{
+    public float shakeDuration = 0.5f;
+    public float shakeAmount = 0.1f;
+    public float decreaseFactor = 1.0f;
+
+    private Vector3 originalPos;
+    private float currentShakeDuration = 0f;
+
+    void Start()
+    {
+        originalPos = transform.position;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+            if (hit.collider != null && hit.collider.gameObject == gameObject)
+            {
+                Shake();
+            }
+        }
+
+        if (currentShakeDuration > 0)
+        {
+            transform.position = originalPos + Random.insideUnitSphere * shakeAmount;
+
+            currentShakeDuration -= Time.deltaTime * decreaseFactor;
+        }
+        else
+        {
+            currentShakeDuration = 0f;
+            transform.position = originalPos;
+        }
+    }
+
+    public void Shake()
+    {
+        currentShakeDuration = shakeDuration;
+    }
+}
--- Assets/Scripts/DestroyItem.cs
+++ Assets/Scripts/DestroyItem.cs
@@ -18,3 +18,4 @@ public class DestroyItem : MonoBehaviour
 }
 
 
+
